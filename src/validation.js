ce803e3de1ffb308e8c264c80534a8026c0e05e2
Olivier Monnier <monnier@indigo-net.com>
2021-04-28 13:57:53 +0200

    clean validation

--- src/validation.js
+++ src/validation.js
@@ -1,40 +1,7 @@
-const isValidObjectWith = fields => obj =>
-  !!obj && !Array.isArray(obj) && fields.every(field => obj.hasOwnProperty(field))
-
-const isValidAction = obj => {
-  return isValidObjectWith(['message', 'func'])(obj) &&
-    typeof obj.func === 'function' && typeof obj.message === 'string'
-}
-
-const isValidActionsArray = arr => arr.every(isValidAction)
-
-const isValidPostParams = obj => {
-  return isValidObjectWith(['message', 'args'])(obj) &&
-    Array.isArray(obj.args) && typeof obj.message === 'string'
-}
-
-const isValidPostParamsArray = arr => arr.every(isValidPostParams)
-
-const isValidObjectsArray = arr => (fields = []) =>
-  arr.every(isValidObjectWith(fields))
-
-const testArray = {
-  'actionsArray': arr => isValidActionsArray(arr),
-  'arraysArray': arr => arr.every(item => Array.isArray(item)),
-  'objectsArray': arr => isValidObjectsArray(arr)(),
-  'postParamsArray': arr => isValidPostParamsArray(arr),
-  'stringsArray': arr => arr.every(item => typeof item === 'string')
-}
-
 const isValidArg = arg => type => {
   if (type === 'null') return arg === null
   if (type === 'undefined') return arg === undefined
-  if (type === 'action') return isValidAction(arg)
-  if (Array.isArray(arg)) {
-    if (type !== 'array' && !testArray[type]) return false
-    if (type === 'array') return true
-    return testArray[type](arg)
-  }
+  if (type === 'array') return true
   if (arg) return typeof arg === type.toString()
   return false
 }
